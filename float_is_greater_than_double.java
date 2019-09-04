6dc3e6c685b075b5e3eb2344668ea52e60ee6e64
loliimay <lolimay@lolimay.cn>
2019-09-04 09:55:27 +0800

    init

---
+++ float_is_greater_than_double.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        float x = 0.4f;
+        double y = 0.4;
+
+        if (x > y) {
+            System.out.println("Hello, it's me!");
+        }
+    }
+}
---
+++ print_the_reference_of_an_object.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        int a[] = { 1, 2, 3 };
+        System.out.println("The reference is: " + a);
+    }
+}
